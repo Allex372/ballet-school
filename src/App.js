de6499841a6aa277fe5517e4d092eb19c8c03ac9
Allex372 <alexalex372.aa@gmail.com>
2021-07-28 15:30:45 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
 import Main_Page from "./components/Main_Page/Main_Page";
 
 function App() {
   return (
-    <div className="App">
-      <Main_Page/>
-    </div>
+    <Router>
+        <Switch>
+            <div className="App">
+                <Route path='/' component={Main_Page}/>
+                <Route path='/home' component={Main_Page}/>
+            </div>
+        </Switch>
+    </Router>
   );
 }
 
