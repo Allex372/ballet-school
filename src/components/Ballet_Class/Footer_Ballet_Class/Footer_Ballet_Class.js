72c77325258d18df9651bfe41097866dcb910eee
Allex372 <alexalex372.aa@gmail.com>
2021-07-29 16:15:38 +0300

    Classes_Page send data (last form)

--- src/components/Ballet_Class/Footer_Ballet_Class/Footer_Ballet_Class.js
+++ src/components/Ballet_Class/Footer_Ballet_Class/Footer_Ballet_Class.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import './footer.css';
 import img from '../../../img/ballet_class/footer_img.jpg';
 
 function Footer_Ballet_Class () {
-  return(
+
+    const [data, setData] = useState({
+        name:'',
+        phone:''
+    })
+
+    function saveDataName(e) {
+        const name = e.target.value;
+        setData(prevState => ({
+            ...prevState,
+            name: name
+        }))
+    }
+
+    function saveDataPhone(e) {
+        const phone = e.target.value;
+        setData(prevState => ({
+            ...prevState,
+            phone: phone
+        }))
+    }
+
+    function sendData() {
+        console.log(data)
+    }
+
+
+
+    return(
       <div className='footer-class-main'>
           <img src={img} alt='background'/>
           <div className='footer-class-wrapper'>
               <p>apply for audition</p>
               <div className='footer-class-form'>
-                  <input type='text' defaultValue='Name'/>
-                  <input type='text' defaultValue='Phone number' style={{marginLeft:'3px'}}/>
-                  <button>Send</button>
+                  <input type='text' defaultValue='Name' onInput={saveDataName}/>
+                  <input type='text' defaultValue='Phone number' onInput={saveDataPhone} style={{marginLeft:'3px'}}/>
+                  <button onClick={sendData}>Send</button>
               </div>
 
           </div>
